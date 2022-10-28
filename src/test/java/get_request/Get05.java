834cd246582a6370b3c4aa247a450083dddc18c8
Abdurrahim Akgün <abdurrahim.akguen@gmail.com>
2022-10-28 23:11:12 +0200

    v1

---
+++ src/test/java/get_request/Get05.java
@@ -0,0 +1,39 @@
+package get_request;
+
+import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get05 extends RestfulBaseUrl {
+
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+      And
+         Among the data there should be someone whose firstname is "Johhny" and lastname is "Dear"
+     */
+
+    @Test
+    public void get01(){
+        //https://restful-booker.herokuapp.com/booking?firstname=Ali&lastname=Cengiz
+        //set the url
+        spec.pathParam("first","booking").queryParams("firstname","Kimie","lastname","Jackie");
+        //2. adim set the expected
+        //3. adim send the request and get the reponse
+        Response response =  given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+        //Do assertion
+        assertEquals(200,response.getStatusCode());
+       assertTrue(response.asString().contains("bookingid"));
+
+    }
+
+
+}
